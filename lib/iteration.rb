72a23dff202ba08d761d9b34e7c85bec4424d270
somaia khalil <sumaya_khair@live.com>
2019-10-25 17:47:38 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,55 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
-end
+results = [ ]
+i = 0 
+while i < src.size do 
+  innerarray = src [i]
+  results << "I love #{innerarray[0]} and #{innerarray [1] } on my pizza"
+  i+=1
+end 
+results
+end 
+
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  results = [ ]
+  i=0 
+  while i < src.size do 
+    innerarray = src [i]
+    if innerarray[0] > innerarray [1]
+      results << innerarray [0]
+    elsif innerarray [1] > innerarray [0]
+     results << innerarray [1]
+   else 
+    results << innerarray[0]
+    end
+    i+=1
+  end
+  results
 end
+# find_greater_pair ([[1,2], [3,4]])
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+# #   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+# #   # if both numbers in the pair are even, then add both those numbers to the
+# #   # total
+# #   #
+# #   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
+# #   # the number was even. Review the operator documentation if you've forgotten
+# #   # this!
+
+i = 0 
+total = 0 
+  while i < src.size do 
+    innerarray = src [i]
+    if innerarray[0]  % 2 == 0 && innerarray [1] % 2 == 0
+      total += innerarray[0] + innerarray [1]
+    else
+      nil
+    end
+     i+=1
+  end 
+  return total
 end
